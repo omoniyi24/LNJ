0ba096a39c3882417a0237158177e4c7ec1488bd
Omoniyi Ilesanmi <omoniyi.ilesanmi@prunny.com>
2022-06-02 21:11:58 +0100

    initialize files

---
+++ src/main/java/com/omoniyi/lnj/LnjApplication.java
@@ -0,0 +1,13 @@
+package com.omoniyi.lnj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnjApplication.class, args);
+	}
+
+}
